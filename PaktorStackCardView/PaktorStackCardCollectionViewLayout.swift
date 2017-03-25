56c710be80c32adf605a7e8f5769731075190bec
muukii <m@muukii.me>
2017-03-26 01:20:32 +0900

    :evergreen_tree: Update

--- PaktorStackCardView/PaktorStackCardCollectionViewLayout.swift
+++ PaktorStackCardView/PaktorStackCardCollectionViewLayout.swift
@@ -12,6 +12,14 @@ public final class PaktorStackCardCollectionViewLayout: UICollectionViewLayout {
 
   // MARK: - Properties
 
+  public func next() {
+    currentIndex += 1
+    collectionView?.performBatchUpdates({
+    }, completion: nil)
+  }
+
+  public var currentIndex: Int = 0
+
   open override class var layoutAttributesClass: Swift.AnyClass {
     return StackCardLayoutAttributes.self
   }
@@ -28,34 +36,43 @@ public final class PaktorStackCardCollectionViewLayout: UICollectionViewLayout {
 
   public override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
 
-    let startItem = 0
-    let frame = collectionView!.bounds.insetBy(dx: 16, dy: 100)
-    let shrink: CGFloat = 0.05
-    let offset: CGFloat = 36
-
-    let attributeses = (0..<3).map { item -> StackCardLayoutAttributes in
-
-      let indexPath = IndexPath(item: startItem + item, section: 0)
-      let diff = shrink * CGFloat(item)
-      let offset = -offset * CGFloat(item)
-      let attributes = StackCardLayoutAttributes(forCellWith: indexPath)
-      attributes.frame = frame
-      attributes.transform = CGAffineTransform(scaleX: 1 - diff, y: 1 - diff).translatedBy(x: 0, y: offset)
-      attributes.zIndex = -item
-      return attributes
-    }
+    let attributeses = (0..<3).flatMap { item in
+      layoutAttributesForItem(at: IndexPath(item: currentIndex + item, section: 0))
+      }
 
     return attributeses
   }
 
   public override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
-    return nil
+
+    let item = indexPath.item - currentIndex
+
+    let shrink: CGFloat = 0.05
+    let offset: CGFloat = 36
+    let frame = collectionView!.bounds.insetBy(dx: 16, dy: 100)
+
+    let indexPath = IndexPath(item: currentIndex + item, section: 0)
+    let diff = shrink * CGFloat(item)
+    let _offset = -offset * CGFloat(item)
+    let attributes = StackCardLayoutAttributes(forCellWith: indexPath)
+    attributes.frame = frame
+    attributes.transform = CGAffineTransform(scaleX: 1 - diff, y: 1 - diff).translatedBy(x: 0, y: _offset)
+    attributes.zIndex = -(indexPath.item)
+
+    if item < 0 {
+      attributes.alpha = 0
+      attributes.isHidden = true
+    } else {
+      attributes.alpha = 1
+      attributes.isHidden = false
+    }
+
+    return attributes
   }
 
   public override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
-    return true
+    return false
   }
-
 }
 
 final class StackCardLayoutAttributes: UICollectionViewLayoutAttributes {
--- PaktorStackCardView/StackCardView.swift
+++ PaktorStackCardView/StackCardView.swift
@@ -8,11 +8,23 @@
 
 import UIKit
 
+public protocol StackCardViewDataSource: class {
+  func numberOfCards(stackCardView: StackCardView) -> Int
+  func stackCardView(_ stackCardView: StackCardView, cardForAt index: Int) -> UIView
+}
+
+public protocol StackCardViewDelegate: class {
+
+}
+
 public final class StackCardView: UIView {
 
-  private lazy var collectionView: UICollectionView = {
-    let layout = PaktorStackCardCollectionViewLayout()
-    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+  public weak var dataSource: StackCardViewDataSource?
+
+  fileprivate let layout = PaktorStackCardCollectionViewLayout()
+
+  fileprivate lazy var collectionView: UICollectionView = {
+    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: self.layout)
     collectionView.backgroundColor = .clear
     collectionView.register(BackdropCell.self, forCellWithReuseIdentifier: "Cell")
     return collectionView
@@ -43,6 +55,10 @@ public final class StackCardView: UIView {
     private lazy var animator: UIDynamicAnimator = UIDynamicAnimator(referenceView: UIApplication.shared.keyWindow!) // FIXME:
     private lazy var behavior: AttachmentBehavior<BackdropCell> = AttachmentBehavior(item: self)
 
+    var didTap: () -> Void = {}
+
+    let label = UILabel()
+
     override init(frame: CGRect) {
 
       super.init(frame: frame)
@@ -50,10 +66,22 @@ public final class StackCardView: UIView {
       let panGesture = UIPanGestureRecognizer(target: self, action: #selector(pan(gesture:)))
       addGestureRecognizer(panGesture)
 
+      label.translatesAutoresizingMaskIntoConstraints = false
+      contentView.addSubview(label)
+
+      label.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+      label.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+
+      let tap = UITapGestureRecognizer(target: self, action: #selector(tap(gesture:)))
+      contentView.addGestureRecognizer(tap)
     }
     
     required init?(coder aDecoder: NSCoder) {
       fatalError("init(coder:) has not been implemented")
+    }
+
+    @objc func tap(gesture: UITapGestureRecognizer) {
+      didTap()
     }
 
     @objc func pan(gesture: UIPanGestureRecognizer) {
@@ -90,13 +118,23 @@ extension StackCardView: UICollectionViewDataSource {
   }
 
   public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return 3
+    return dataSource?.numberOfCards(stackCardView: self) ?? 0
   }
 
   public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    print(indexPath)
+
+    guard let card = dataSource?.stackCardView(self, cardForAt: indexPath.item) else {
+      return UICollectionViewCell()
+    }
+
     let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! BackdropCell
     #if DEBUG
       cell.contentView.backgroundColor = UIColor(white: 0.7, alpha: 0.5)
+      cell.label.text = String(describing: indexPath)
+      cell.didTap = { [weak self] in
+        self?.layout.next()
+      }
     #endif
     return cell
   }
@@ -121,16 +159,15 @@ final class AttachmentBehavior<T: UIDynamicItem>: UIDynamicBehavior {
     let point = item.center
     attachmentBehavior = UIAttachmentBehavior(item: item, attachedToAnchor: point)
     attachmentBehavior.frequency = 4
-    attachmentBehavior.damping = 0.4
+    attachmentBehavior.damping = 0.8
     attachmentBehavior.length = 0
     attachmentBehavior.action = { [unowned item] in
-      // TODO: ここでscale ?
-      print(item.center)
+
     }
 
     itemBehavior = UIDynamicItemBehavior(items: [item])
-    itemBehavior.density = 100
-    itemBehavior.resistance = 25
+    itemBehavior.density = 300
+    itemBehavior.resistance = 50
 
     super.init()
 
